cc03dcc9b85fd5645bb217b3f217d7f703e05298
Lourenco <lomadara@gmail.com>
2023-04-30 12:09:33 -0300

    reutilização de header e footer a partir de um arquivo base.html, criação de pagina sobre e inicio da criação do formulario de pesquisa de leilões

---
+++ src/constants/property_types_constant.py
@@ -0,0 +1,42 @@
+property_types = [
+    {
+        "id":1,
+        "text":"Apartamento"
+    },
+    {
+        "id":2,
+        "text":"Casa"
+    },
+    {
+        "id":3,
+        "text":"Casa em condomínio"
+    },
+    {
+        "id":4,
+        "text":"Terreno"
+    },
+    {
+        "id":5,
+        "text":"Chácara/Sitio"
+    },
+    {
+        "id":6,
+        "text":"Fazenda"
+    },
+    {
+        "id":7,
+        "text":"Galpão"
+    },
+    {
+        "id":8,
+        "text":"Sala Comercial"
+    },
+    {
+        "id":9,
+        "text":"Salão Comercial"
+    },
+    {
+        "id":10,
+        "text":"Loja"
+    }
+]
---
+++ src/constants/states_constant.py
@@ -0,0 +1,110 @@
+states = [
+    {
+        "id":1,
+        "text":"Acre"
+    },
+    {
+        "id":2,
+        "text":"Alagoas"
+    },
+    {
+        "id":3,
+        "text":"Amazonas"
+    },
+    {
+        "id":4,
+        "text":"Amapá"
+    },
+    {
+        "id":5,
+        "text":"Bahia"
+    },
+    {
+        "id":6,
+        "text":"Ceará"
+    },
+    {
+        "id":7,
+        "text":"Distrito Federal"
+    },
+    {
+        "id":8,
+        "text":"Espírito Santo"
+    },
+    {
+        "id":9,
+        "text":"Goiás"
+    },
+    {
+        "id":10,
+        "text":"Maranhão"
+    },
+    {
+        "id":11,
+        "text":"Minas Gerais"
+    },
+    {
+        "id":12,
+        "text":"Mato Grosso do Sul"
+    },
+    {
+        "id":13,
+        "text":"Mato Grosso"
+    },
+    {
+        "id":14,
+        "text":"Pará"
+    },
+    {
+        "id":15,
+        "text":"Paraíba"
+    },
+    {
+        "id":16,
+        "text":"Pernambuco"
+    },
+    {
+        "id":17,
+        "text":"Piauí"
+    },
+    {
+        "id":18,
+        "text":"Paraná"
+    },
+    {
+        "id":19,
+        "text":"Rio de Janeiro"
+    },
+    {
+        "id":20,
+        "text":"Rio Grande do Norte"
+    },
+    {
+        "id":21,
+        "text":"Rondônia"
+    },
+    {
+        "id":22,
+        "text":"Roraima"
+    },
+    {
+        "id":23,
+        "text":"Rio Grande do Sul"
+    },
+    {
+        "id":24,
+        "text":"Santa Catarina"
+    },
+    {
+        "id":25,
+        "text":"Sergipe"
+    },
+    {
+        "id":26,
+        "text":"São Paulo"
+    },
+    {
+        "id":27,
+        "text":"Tocantins"
+    }
+]
